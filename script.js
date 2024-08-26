176b6cb4210ca06831ac6ac0e0bb134337f5cf64
QuizLeapsBooks <167664682+QuizLeapsBooks@users.noreply.github.com>
2024-08-26 12:45:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const now = new Date().getTime();
+    function checkTime() {
+        const now = new Date();
+        const hours = now.getHours();
+        const minutes = now.getMinutes();
 
-    const countDownDate = now + (2 * 24 * 60 * 60 * 1000) + (20 * 60 * 60 * 1000) + (30 * 60 * 1000);
-
-    const x = setInterval(function() {
-        const current = new Date().getTime();
-        
-        const distance = countDownDate - current;
-        
-        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        
-        document.getElementById("days").innerHTML = days;
-        document.getElementById("hours").innerHTML = hours;
-        document.getElementById("minutes").innerHTML = minutes;
-        document.getElementById("seconds").innerHTML = seconds;
-        
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("countdown").innerHTML = "EXPIRED";
-            window.location.href = "#"; 
+        if (hours === 1 && minutes === 0) {
+            window.location.href = 'https://quizleapsbooks.github.io/QuizLeapBooks-Test_Started/';
+        } else {
+            setTimeout(checkTime, 60000);
         }
-    }, 1000);
+    }
+
+    checkTime(); 
 });
